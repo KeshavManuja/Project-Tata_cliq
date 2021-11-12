326404ac70d61d054d120f7e3802a2f0fb6194c8
KeshavManuja <93030991+KeshavManuja@users.noreply.github.com>
2021-11-12 19:02:55 +0530

    Add files via upload

---
+++ brands.js
@@ -0,0 +1,262 @@
+
+var brandArray=[
+    [
+      '<tr>  <th><a href="#">Popular brands</a></th>  <th><a href="#">Featured brands</a></th>   </tr>',
+      '<tr>  <td><a href="#">LG</a></td>  <td><a href="#">VU</a></td>   </tr>',
+      '<tr>  <td><a href="#">Samsung</a></td>  <td><a href="#">Lloyd</a></td>   </tr>',
+      '<tr>  <td><a href="#">Sony</a></td>  <td><a href="#">TCL</a></td>   </tr>',
+      '<tr>  <td><a href="#">Hitachi</a></td>  <td><a href="#">Sanyo</a></td>   </tr>',
+      '<tr>  <td><a href="#">Daikin</a></td>  <td><a href="#">Godrej</a></td>   </tr>',
+      '<tr>  <td><a href="#">Carrier</a></td>  <td><a href="#">Prestige</a></td>   </tr>',
+      '<tr>  <td><a href="#">Blue Star</a></td>  <td><a href="#">Wonderchef</a></td>   </tr>',
+      '<tr>  <td><a href="#">Bosch</a></td>  <td><a href="#">Tata Swach</a></td>   </tr>',
+      '<tr>  <td><a href="#">Xiaomi</a></td>  <td><a href="#">Livpure</a></td>   </tr>',
+      '<tr>  <td><a href="#">Panasonic</a></td>  <td><a href="#">Luminous</a></td>   </tr>',
+      '<tr>  <td><a href="#">Bajaj</a></td>  <td><a href="#">KitchenAid</a></td>   </tr>',
+      '<tr>  <td><a href="#">Kent</a></td>  <td><a href="#">Syska</a></td>   </tr>',
+      '<tr>  <td><a href="#">Havells</a></td>  <td><a href="#">Symphony</a></td>   </tr>'
+    ],
+    [
+      '<tr>  <th><a href="#">Popular brands</a></th>  <th><a href="#">Featured brands</a></th>   </tr>',
+      '<tr>  <td><a href="#">Apple</a></td>  <td><a href="#">Nokia</a></td>   </tr>',
+      '<tr>  <td><a href="#">Oppo</a></td>  <td><a href="#">Motorola</a></td>   </tr>',
+      '<tr>  <td><a href="#">Xiaomi</a></td>  <td><a href="#">Boult Audio</a></td>   </tr>',
+      '<tr>  <td><a href="#">HP</a></td>  <td><a href="#">Acer</a></td>   </tr>',
+      '<tr>  <td><a href="#">Dell</a></td>  <td><a href="#">Microsoft</a></td>   </tr>',
+      '<tr>  <td><a href="#">Asus</a></td>  <td><a href="#">Noise</a></td>   </tr>',
+      '<tr>  <td><a href="#">Canon</a></td>  <td><a href="#">Ambrane</a></td>   </tr>',
+      '<tr>  <td><a href="#">Nikon</a></td>  <td><a href="#">Zebronics</a></td>   </tr>',
+      '<tr>  <td><a href="#">GoPro</a></td>  <td><a href="#">Epson</a></td>   </tr>',
+      '<tr>  <td><a href="#">Fitbit</a></td>  <td><a href="#">Kingston</a></td>   </tr>',
+      '<tr>  <td><a href="#">Google</a></td>  <td><a href="#">Blaupunkt</a></td>   </tr>',
+      '<tr>  <td><a href="#">Seagate</a></td>  <td><a href="#">Sennheiser</a></td>   </tr>',
+      '<tr>  <td><a href="#">WD</a></td>   </tr>',
+      '<tr>  <td><a href="#">SanDisk</a></td>   </tr>'
+    ],
+    [
+      '<tr>  <th><a href="#">Popular brands</a></th>  <th><a href="#">Featured brands</a></th>   </tr>',
+      '<tr>  <td><a href="#">Utsa</a></td>  <td><a href="#">Westside</a></td>   </tr>',
+      '<tr>  <td><a href="#">Jaipur Kurti</a></td>  <td><a href="#">W</a></td>   </tr>',
+      '<tr>  <td><a href="#">Bombay Paisley</a></td>  <td><a href="#">Aurelia</a></td>   </tr>',
+      '<tr>  <td><a href="#">Aks</a></td>  <td><a href="#">Nuon</a></td>   </tr>',
+      '<tr>  <td><a href="#">Zuba</a></td>  <td><a href="#">Enamor</a></td>   </tr>',
+      '<tr>  <td><a href="#">Globus</a></td>  <td><a href="#">United Color of Benetton</a></td>   </tr>',
+      '<tr>  <td><a href="#">Global Desi</a></td>  <td><a href="#">Forever 21</a></td>   </tr>',
+      '<tr>  <td><a href="#">Ethnicity</a></td>  <td><a href="#">YamamaY</a></td>   </tr>',
+      '<tr>  <td><a href="#">Vark</a></td>  <td><a href="#">Gia</a></td>   </tr>',
+      '<tr>  <td><a href="#">Van Heusen</a></td>  <td><a href="#">Hunkemoller</a></td>   </tr>',
+      '<tr>  <td><a href="#">Pantaloons</a></td>  <td><a href="#">Etam Paris</a></td>   </tr>',
+      '<tr>  <td><a href="#">Label Ritu Kumar</a></td>  <td><a href="#">Wunderlove</a></td>   </tr>',
+      '<tr>  <td><a href="#"></a></td>  <td><a href="#">Soch</a></td>   </tr>',
+      '<tr>  <td><a href="#"></a></td>  <td><a href="#">Kazo</a></td>   </tr>'
+    ],
+    [
+      '<tr>  <th><a href="#">Popular brands</a></th>  <th><a href="#">Featured brands</a></th>   </tr>',
+      '<tr>  <td><a href="#">ETA</a></td>  <td><a href="#">Westside</a></td>   </tr>',
+      '<tr>  <td><a href="#">Nuon Men</a></td>  <td><a href="#">U.S. Polo Assn</a></td>   </tr>',
+      '<tr>  <td><a href="#">Pepe Jeans</a></td>  <td><a href="#">WES</a></td>   </tr>',
+      '<tr>  <td><a href="#">Puma</a></td>  <td><a href="#">Peter England</a></td>   </tr>',
+      '<tr>  <td><a href="#">Park Avenue</a></td>  <td><a href="#">Fruit of the Loom</a></td>   </tr>',
+      '<tr>  <td><a href="#">Ascot</a></td>  <td><a href="#">Bewakoof</a></td>   </tr>',
+      '<tr>  <td><a href="#">Killer</a></td>  <td><a href="#">Flying Machine</a></td>   </tr>',
+      '<tr>  <td><a href="#">Levis</a></td>  <td><a href="#">Celio</a></td>   </tr>',
+      '<tr>  <td><a href="#">Raymond</a></td>  <td><a href="#">Westsport</a></td>   </tr>',
+      '<tr>  <td><a href="#">Blackberrys</a></td>  <td><a href="#">Parx</a></td>   </tr>',
+      '<tr>  <td><a href="#">Woodland</a></td>  <td><a href="#">Spykar</a></td>   </tr>',
+      '<tr>  <td><a href="#"></a></td>  <td><a href="#">ANDAMEN</a></td>   </tr>'
+    ],
+    [
+      '<tr>  <th><a href="#">Popular brands</a></th>  <th><a href="#">Featured brands</a></th>   </tr>',
+      '<tr>  <td><a href="#">Red Chief</a></td>  <td><a href="#">Red Tape</a></td>   </tr>',
+      '<tr>  <td><a href="#">Ruosh</a></td>  <td><a href="#">Puma</a></td>   </tr>',
+      '<tr>  <td><a href="#">Bugatti</a></td>  <td><a href="#">Crocs</a></td>   </tr>',
+      '<tr>  <td><a href="#">Alberto Torresi</a></td>  <td><a href="#">Asics</a></td>   </tr>',
+      '<tr>  <td><a href="#">Adidas</a></td>  <td><a href="#">Woodland</a></td>   </tr>',
+      '<tr>  <td><a href="#">LUNA BLU</a></td>  <td><a href="#">Catwalk</a></td>   </tr>',
+      '<tr>  <td><a href="#">Regal</a></td>  <td><a href="#">Clarks</a></td>   </tr>',
+      '<tr>  <td><a href="#">Reebok</a></td>  <td><a href="#">Pavers England</a></td>   </tr>',
+      '<tr>  <td><a href="#">Carlton London</a></td>  <td><a href="#">Metro</a></td>   </tr>',
+      '<tr>  <td><a href="#">Aldo</a></td>  <td><a href="#">Mochi</a></td>   </tr>',
+      '<tr>  <td><a href="#">Lotto</a></td>  <td><a href="#">Bata</a></td>   </tr>',
+      '<tr>  <td><a href="#">Hush Puppies</a></td>  <td><a href="#">Skechers</a></td>   </tr>',
+      '<tr>  <td><a href="#">Salomon</a></td>  <td><a href="#">Franco Leone</a></td>   </tr>',
+      '<tr>  <td><a href="#">Altra</a></td>  <td><a href="#">FILA</a></td>   </tr>'
+    ],
+    [
+      '<tr>  <th><a href="#">Popular brands</a></th>  <th><a href="#">Featured brands</a></th>   </tr>',
+      '<tr>  <td><a href="#">Tanishq</a></td>  <td><a href="#">Mellora</a></td>   </tr>',
+      '<tr>  <td><a href="#">Mia by Tanishq</a></td>  <td><a href="#">Shaya</a></td>   </tr>',
+      '<tr>  <td><a href="#">Malabar</a></td>  <td><a href="#">Clara</a></td>   </tr>',
+      '<tr>  <td><a href="#">Joyalukkas</a></td>  <td><a href="#">Sri Jagdamba Pearls</a></td>   </tr>',
+      '<tr>  <td><a href="#">P. C. Jewellers</a></td>  <td><a href="#">Tribe Amrapali</a></td>   </tr>',
+      '<tr>  <td><a href="#">P.C. Chandra Jewellers</a></td>  <td><a href="#">Izaara</a></td>   </tr>',
+      '<tr>  <td><a href="#">P.N. Gadgil Jewellers</a></td>  <td><a href="#">Taraash</a></td>   </tr>',
+      '<tr>  <td><a href="#">Waman Hari Pethe</a></td>  <td><a href="#">Ahilya Jewels</a></td>   </tr>',
+      '<tr>  <td><a href="#">Orra</a></td>  <td><a href="#">Talisman</a></td>   </tr>',
+      '<tr>  <td><a href="#">Bangalore Refinery</a></td>  <td><a href="#">Accessorize London</a></td>   </tr>',
+      '<tr>  <td><a href="#"></a></td>  <td><a href="#">The Real Effect London</a></td>   </tr>'
+    ],
+    [
+      '<tr>  <th><a href="#">Popular brands</a></th>  <th><a href="#">Featured brands</a></th>   </tr>',
+      '<tr>  <td><a href="#">Lavie</a></td>  <td><a href="#">Casio</a></td>   </tr>',
+      '<tr>  <td><a href="#">Caprese</a></td>  <td><a href="#">Tommy Hilfiger</a></td>   </tr>',
+      '<tr>  <td><a href="#">Hidesign</a></td>  <td><a href="#">Sonata</a></td>   </tr>',
+      '<tr>  <td><a href="#">Lino Perros</a></td>  <td><a href="#">Daniel Wellington</a></td>   </tr>',
+      '<tr>  <td><a href="#">Baggit</a></td>  <td><a href="#">Timex</a></td>   </tr>',
+      '<tr>  <td><a href="#">American Tourister</a></td>  <td><a href="#">Guess Collection</a></td>   </tr>',
+      '<tr>  <td><a href="#">Carlton London</a></td>  <td><a href="#">Citizen</a></td>   </tr>',
+      '<tr>  <td><a href="#">Skybags</a></td>  <td><a href="#">Anne Klein</a></td>   </tr>',
+      '<tr>  <td><a href="#">Giordano & Gio</a></td>  <td><a href="#">Kenneth Cole</a></td>   </tr>',
+      '<tr>  <td><a href="#">Esbeda</a></td>  <td><a href="#">Giordano</a></td>   </tr>',
+      '<tr>  <td><a href="#">Fossil</a></td>  <td><a href="#">Gio Collection</a></td>   </tr>',
+      '<tr>  <td><a href="#">Da Milano</a></td>  <td><a href="#">Victorinox</a></td>   </tr>',
+      '<tr>  <td><a href="#">Fastrack</a></td>  <td><a href="#"></a></td>   </tr>',
+      '<tr>  <td><a href="#">Elle</a></td>  <td><a href="#"></a></td>   </tr>',
+      '<tr>  <td><a href="#">Addons</a></td>  <td><a href="#"></a></td>   </tr>'
+    ],
+    [
+      '<tr>  <th><a href="#">Popular brands</a></th>  <th><a href="#">Featured brands</a></th>   </tr>',
+      '<tr>  <td><a href="#">Allen Solly Junior</a></td>  <td><a href="#">Cutecumber</a></td>   </tr>',
+      '<tr>  <td><a href="#">United Colors of Benetton</a></td>  <td><a href="#">Peppermint</a></td>   </tr>',
+      '<tr>  <td><a href="#">Gini & Jony</a></td>  <td><a href="#">A Little Fable</a></td>   </tr>',
+      '<tr>  <td><a href="#">Pantaloons Junior</a></td>  <td><a href="#">Miniklub</a></td>   </tr>',
+      '<tr>  <td><a href="#">Utsa Kids by Westside</a></td>  <td><a href="#">HOP by Westside</a></td>   </tr>',
+      '<tr>  <td><a href="#">Global Desi Girl</a></td>  <td><a href="#">Y&F by Westside</a></td>   </tr>',
+      '<tr>  <td><a href="#">Pepe Jeans</a></td>  <td><a href="#">Nauti Nati</a></td>   </tr>',
+      '<tr>  <td><a href="#">Pantaloons Baby</a></td>  <td><a href="#">One Friday</a></td>   </tr>',
+      '<tr>  <td><a href="#">Crocs</a></td>  <td><a href="#">612 League</a></td>   </tr>',
+    ]
+  ]
+
+
+
+
+
+
+
+
+ var navBrand=document.querySelector(".brand")
+
+ navBrand.onmouseover=function(){
+     document.getElementById("blockshow").style.display="flex";
+     table.innerHTML=brandArray[0].join('')
+     navBrand.style.color="black"
+     navBrand.style.backgroundColor="white"
+     var aroicon=document.querySelector("#farb")
+    aroicon.style.transform="rotate(180deg)"
+    brandArrow=0;
+    arrowbrand(brandArrow)
+ }
+
+ navBrand.onmouseout=function(){
+    document.getElementById("blockshow").style.display="none";
+    navBrand.style.color="white"
+    navBrand.style.backgroundColor="#212121"
+    var aroicon=document.querySelector("#farb")
+    aroicon.style.transform="rotate(0deg)"
+    
+}
+
+var blockshow= document.getElementById("blockshow")
+
+
+blockshow.onmouseover=function(){
+    document.getElementById("blockshow").style.display="flex";
+    navBrand.style.color="black"
+    navBrand.style.backgroundColor="white"
+    var aroicon=document.querySelector("#farb")
+    aroicon.style.transform="rotate(180deg)"
+   
+    
+ }
+
+blockshow.onmouseout=function(){
+   document.getElementById("blockshow").style.display="none";
+   navBrand.style.color="white"
+   navBrand.style.backgroundColor="#212121"
+   var aroicon=document.querySelector("#farb")
+   aroicon.style.transform="rotate(0deg)"
+   
+}
+
+
+var table= document.getElementById("brandTable")
+
+var brandTv=document.getElementById("brandTv")
+var brandElc=document.getElementById("brandElc")
+var brandWomen=document.getElementById("brandWomen")
+var brandMen=document.getElementById("brandMen")
+var brandFoot=document.getElementById("brandFoot")
+var brandJwel=document.getElementById("brandJwel")
+var brandwatch=document.getElementById("brandwatch")
+
+var brandKid=document.getElementById("brandKid")
+
+var brandArrow=100;
+var disBrandarr=["navbrn1","navbrn2", "navbrn3", "navbrn4", "navbrn5", "navbrn6", "navbrn7", "navbrn8"]
+var disBrandarr2=["brandTv", "brandElc", "brandWomen", "brandMen", "brandFoot", "brandJwel","brandwatch", "brandKid"]
+
+function arrowbrand(brandArrow ){
+  for(var b=0; b<8; b++)
+  {
+    if(brandArrow==b){
+        var brantemp=document.getElementById(disBrandarr[b])
+        brantemp.style.display="block"
+      var distemp1=document.getElementById(disBrandarr2[b])
+      distemp1.style.color="black"
+
+    }
+    else{
+      var brantemp=document.getElementById(disBrandarr[b])
+        brantemp.style.display="none"
+        var distemp1=document.getElementById(disBrandarr2[b])
+      distemp1.style.color="gray"
+        
+    }
+  }
+}
+
+
+
+
+ brandTv.onmouseover=function(){
+     table.innerHTML=brandArray[0].join('')
+     brandArrow=0;
+    arrowbrand(brandArrow)
+ }
+ brandElc.onmouseover=function(){
+     table.innerHTML=brandArray[1].join('')
+     brandArrow=1;
+    arrowbrand(brandArrow)
+
+ }
+ brandWomen.onmouseover=function(){
+     table.innerHTML=brandArray[2].join('')
+     brandArrow=2;
+     arrowbrand(brandArrow)
+ }
+ brandMen.onmouseover=function(){
+     table.innerHTML=brandArray[3].join('')
+     brandArrow=3;
+     arrowbrand(brandArrow)
+ }
+ brandFoot.onmouseover=function(){
+     table.innerHTML=brandArray[4].join('')
+     brandArrow=4;
+     arrowbrand(brandArrow)
+ }
+ brandJwel.onmouseover=function(){
+     table.innerHTML=brandArray[5].join('')
+     brandArrow=5;
+     arrowbrand(brandArrow)
+ }
+ brandwatch.onmouseover=function(){
+     table.innerHTML=brandArray[6].join('')
+     brandArrow=6;
+     arrowbrand(brandArrow)
+ }
+ brandKid.onmouseover=function(){
+     table.innerHTML=brandArray[7].join('')
+     brandArrow=7;
+     arrowbrand(brandArrow)
+ }
